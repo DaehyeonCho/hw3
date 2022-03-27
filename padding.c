403718326f10fb919a47814993091e393e4a8634
wheogus4629 <69081057+wheogus4629@users.noreply.github.com>
2022-03-27 17:10:09 +0900

    Add files via upload

---
+++ padding.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct student{         // student 구조체 선언
+    char lastname[13];  // 13바이트
+    int studetID;       // 4바이트
+    short grade;        // 2바이트
+};
+
+int main(){
+    printf("[----- [Cho Daehyeon] [2016039075] -----]\n");
+    struct student pst; // pst student 구조체 생성
+
+    printf("size of student = %1d\n", sizeof(struct student));  // student 구조체 크기 출력(24바이트)
+    printf("size of int = %1d\n", sizeof(int));                 // int 자료형 크기 출력(4바이트)
+    printf("size of short = %1d\n", sizeof(short));             // short 자료형 크기 출력(2바이트)
+
+    return 0;
+}
